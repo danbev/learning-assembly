c1cc50927cbc7d40eddfef3c57bdd66042c017b9
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-11-12 09:21:22 +0100

    link with -no_pie

--- gas/learning_i386.s
+++ gas/learning_i386.s
@@ -4,18 +4,20 @@
 .globl _main
 
 _main:
-    push %ebp
-    mov %esp, %ebp
+    pushl %ebp        # push the value of ebp onto the stack, to save 
+    movl %esp, %ebp   # store the current stack pointer in ebp to we can use ebp
+                      # with indirect addressing
     
-    sub $0x4, %esp
-    push $msg
+    subl $0x4, %esp   # make room for a 32 bit value on the stack to avoid overwriting
+                      # the return address on the stack
+    pushl $msg        # push msg onto the stack (will increment esp but ebp will remain
     call _puts
-    add $0x8, %esp
+    addl $0x8, %esp   # clean up the stack
     
-    mov $1, %eax
-    pop %ebp
+    movl $1, %eax
+    popl %ebp
     ret
 
-.data
+.section __DATA, __data
 msg:
     .ascii "Learning Assembler\0"
