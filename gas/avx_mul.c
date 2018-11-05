c3865ee0d43f32a63774229958167e5493b06590
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-11-05 09:54:44 +0100

    src: change array names to a and b

--- gas/avx_mul.c
+++ gas/avx_mul.c
@@ -4,10 +4,10 @@
 int main() {
 
   // set as big endian (r = revers of default little endian)
-  __m256d evens = _mm256_setr_pd(1.0, 2.0, 3.0, 4.0);
-  __m256d odds = _mm256_setr_pd(1.0, 2.0, 3.0, 4.0);
+  __m256d a = _mm256_setr_pd(1.0, 2.0, 3.0, 4.0);
+  __m256d b = _mm256_setr_pd(1.0, 2.0, 3.0, 4.0);
 
-  __m256d result = _mm256_mul_pd(evens, odds);
+  __m256d result = _mm256_mul_pd(a, b);
 
   double* d = (double*)&result;
   printf("%f %f %f %f\n", d[0], d[1], d[2], d[3]);
