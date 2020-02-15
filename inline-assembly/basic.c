e14a5fda39aec5d16f486b37a46325a4d8f0a4c1
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-02-15 07:24:34 +0100

    src: add multiple inputs example

--- inline-assembly/basic.c
+++ inline-assembly/basic.c
@@ -4,12 +4,13 @@ int main(int argc, char** argv) {
     printf("main...\n");
 
     int x = 10;
-    int y;
+    int y = 20;
+    int z;
     asm("mov %1, %0\n\t"
-        "add $3, %0"
-        : "=r" (y)
-        : "r" (x));
+        "add %2, %0"
+        : "=r" (z)
+        : "r" (x), "r" (y));
 
-    printf("x=%d, y=%d\n", x, y);
+    printf("x=%d, y=%d, z=%d\n", x, y, z);
     return 0;
 }
