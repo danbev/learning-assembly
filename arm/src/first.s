e8ca1fa0ed0d16d171ac691d1d55821f10ec9f59
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-02-01 12:12:35 +0100

    src: clean up of first.s

--- arm/src/first.s
+++ arm/src/first.s
@@ -1,7 +1,7 @@
 .data
 
 msg:
-    .ascii        "Hello, ARM64!\n"
+    .ascii        "Bajja\n"
 len = . - msg
 
 .text
@@ -9,13 +9,13 @@ len = . - msg
 .globl _start
 _start:
     /* syscall write(int fd, const void *buf, size_t count) */
-    mov     x0, #1      /* fd := STDOUT_FILENO */
-    ldr     x1, =msg    /* buf := msg */
-    ldr     x2, =len    /* count := len */
-    mov     w8, #64     /* write is syscall #64 */
-    svc     #0          /* invoke syscall */
+    mov     x0, #1      /* fd */
+    ldr     x1, =msg    /* buf */
+    ldr     x2, =len    /* count */
+    mov     w8, #64     /* write syscall #64 */
+    svc     #0          
 
     /* syscall exit(int status) */
-    mov     x0, #0      /* status := 0 */
-    mov     w8, #93     /* exit is syscall #1 */
-    svc     #0          /* invoke syscall */
+    mov     x0, #0      /* status */
+    mov     w8, #93     /* exit syscall #93 */
+    svc     #0          
