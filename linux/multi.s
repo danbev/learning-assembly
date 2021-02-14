eca3a8e014ceac8cb615bca01f271f516381354a
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-02-14 14:25:29 +0100

    src: write result of multiplication

--- linux/multi.s
+++ linux/multi.s
@@ -3,9 +3,17 @@
 .global _start
 
 _start:
-  mov $8, %ax
-  mov $4, %bx
+  mov $4, %ax
+  mov $2, %bx
   mul %bx
+  add $48, %rax /* add ascii '0' which is decimal 48 so  we can print it */
+  push %rax
+
+  mov $1, %rax
+  mov $1, %rdi
+  lea (%rsp), %rsi
+  mov $1, %dx
+  syscall
 
   mov $60, %rax
   mov $0, %rdi
