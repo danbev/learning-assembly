f28a84c61c22eb93b70066e5bedb814132ed091a
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-02-18 09:16:24 +0100

    src: add executable .data section example

---
+++ linux/exec-stack.s
@@ -0,0 +1,16 @@
+.data
+code: .long 0xCC
+
+.text
+.global _start
+
+_start:
+  jmp code
+
+  mov $60, %rax
+  mov $0, %rdi
+  syscall
+
+/* Notice that without the following directive it is not possible to execute
+   instructions in the data section */
+#.section .note.GNU-stack,"",@progbits
