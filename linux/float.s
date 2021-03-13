25d5735b69330b5befddc348c2ee3cdb15cb82ec
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-03-13 16:35:04 +0100

    src: update float.s with more examples

--- linux/float.s
+++ linux/float.s
@@ -1,19 +1,23 @@
 .global _start
 
 .data
-  radius: .float 3.14
+  radius: .float 3.14   # .float is 64 bits
   m: .float 2.1
   result: .float 0
+  s: .short             # .short is 32 bits
+  age: .word 46
 
 .text
 _start:
   nop
   mov %rsp, %rbp
 
-  fld radius # load onto the FPU stack
-  fld m # load onto the FPU stack
-  fmulp
-  fstpl result # store floating point value in result
+  fld radius    # load onto the FPU stack
+  fld m         # load onto the FPU stack
+  fmulp         #
+  fstp result   # store floating point value in result and pop the stack
+
+  fild age      # load integer and convert to floating point, then push onto the stack
 
   mov $60, %rax
   mov $1, %rdi
