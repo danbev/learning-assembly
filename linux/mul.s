5ee907421a6df471819c5207b3e60aa6e817c196
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-06-07 11:35:05 +0200

    src: show usage of imul with single reg

--- linux/mul.s
+++ linux/mul.s
@@ -2,17 +2,17 @@
 .global _start
 
 _start:
-  mov $33, %rax
-  mov $2, %rbx
-  imul %rax, %rbx
+  mov $33, %al
+  mov $2, %dl
+  imul %dl
 
-  push %rbx
+  push %rax
 
   mov $1, %rax
   mov $1, %rdi
   lea (%rsp), %rsi
   mov $1, %dx
-  syscall         // will bring B which is the ascii character for 66
+  syscall         # Will print B which is the ascii character for 66
 
   mov $60, %rax
   mov $1, %rdi
